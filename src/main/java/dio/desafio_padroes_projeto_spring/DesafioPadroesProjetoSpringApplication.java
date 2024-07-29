ca38db6a246a7c7b5ce4bde9b85457073f2c0e43
rewerp <rewer_p@hotmail.com>
2024-07-29 11:29:28 -0300

    Add: template do projeto para iniciar

---
+++ src/main/java/dio/desafio_padroes_projeto_spring/DesafioPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.desafio_padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/desafio_padroes_projeto_spring/DesafioPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio_padroes_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
